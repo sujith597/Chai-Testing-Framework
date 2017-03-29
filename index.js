8773bd6f0e80c192590752d92fe3a28b4988f444
sujith Kumar <coder@sujithkumar.in>
2017-03-29 13:53:20 +0530

    Updated project details and UI

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿var chaiApp = angular.module("chaiApp", ['ui.router']);
+chaiApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/Home");
+
+    $stateProvider
+    .state('Home', {
+        url: '/Home',
+        templateUrl: 'Application/Chai/chai.html'
+    })
+    .state('Chai', {
+        url: '/Chai',
+        templateUrl: 'Application/Chai/chai.html'
+    })
+    .state('Mocha', {
+        url: '/Mocha',
+        templateUrl: 'Application/Mocha/Mocha.html'
+    })
+    .state('App', {
+        url: '/App',
+        templateUrl: 'Application/App/App.html'
+    })
+});
